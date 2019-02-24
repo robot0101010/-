d5954cae1aae09c622089760fda858a22d91a901
robot0101010 <tt.tt.tt.tt.tt.tt.tt@yandex.ru>
2019-02-24 05:26:00 +0300

    no message

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,30 +1,17 @@
+
+$(document).ready(function(){
+  $(document).ready(function(){
+
 var $homeSlider = $(".home-slider");
 
-$(window).resize(function() {
-  showHomeSlider();
+$homeSlider.addClass("owl-carousel").owlCarousel({
+items: 5,
+loop: true,
+autoplay: true,
+autoplayTimeout: 2000,
+smartSpeed: 1000
 });
 
-function showHomeSlider() {
-  if ($homeSlider.data("owlCarousel") !== "undefined") {
-    if (window.innerWidth <= 600) {
-      initialHomeSlider();
-    } else {
-      destroyHomeSlider();
-    }
-  }
-}
-showHomeSlider();
+})
 
-function initialHomeSlider() {
-  $homeSlider.addClass("owl-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    smartSpeed: 1000
-  });
-}
-
-function destroyHomeSlider() {
-  $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
 }
