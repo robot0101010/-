a5a6051d15fc5fb73944fd954e29a11bb31040b1
robot0101010 <tt.tt.tt.tt.tt.tt.tt@yandex.ru>
2019-02-25 01:37:15 +0300

    no message

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 var $homeSlider = $(".home-slider");
 
-$homeSlider.addClass("owl-carousel").owlCarousel({
-items: 5,
+$homeSlider.addClass(".owl-carousel").owlCarousel({
+items: 2,
 loop: true,
 autoplay: true,
 autoplayTimeout: 2000,
