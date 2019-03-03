2e0d2bd0a4eb23fbfccb8b3adf8466fb9578f378
robot0101010 <tt.tt.tt.tt.tt.tt.tt@yandex.ru>
2019-03-03 20:56:45 +0300

    no message

--- js/application.js
+++ js/application.js
@@ -1,13 +1,16 @@
+
 $(document).ready(function(){
 
-var $homeSlider = $(".home-slider");
+    var $homeSlider = $(".home-slider");
 
-$homeSlider.addClass(".owl-carousel").owlCarousel({
-items: 2,
-loop: true,
-autoplay: true,
-autoplayTimeout: 2000,
-smartSpeed: 1000
+    $homeSlider.addClass(".owl-carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 5,
+        stagePadding: 50,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        smartSpeed: 1000
+    });
+    
 });
-
-})
