006a06199c3b383f309fdfffd94addd804ae0bec
robot0101010 <tt.tt.tt.tt.tt.tt.tt@yandex.ru>
2019-03-04 22:14:54 +0300

    no message

--- js/application.js
+++ js/application.js
@@ -19,7 +19,40 @@ $(document).ready(function(){
     $(".ul_element").slideToggle(300)
     $(".ssssss").css("font-size","0.7em");
 	});
-
-	
-     
 });
+
+$(function(){
+    //cоздание обьекта "Modal"
+    var modal = {
+        self: $('#modal'),  //модульное окно
+
+        showModal: function(content){
+            this.self.find('#innerModal').html(content);
+            this.self.fadeIn(200);
+        },
+        hideModal: function(){        //спрятать окно
+            this.self.fadeOut(200);
+            this.self.find('#innerModal').html('');
+        }
+
+    };
+    //обработка события нажатия кнопки ' show modal
+
+    $('.showModal').on("click",function(e){
+        var id = $(this).data('id');
+        var content = $('#cont'+id).html();
+        modal.showModal(content);
+});
+    //обработка события клика на само модульное окно
+    $('#modal')on.('click',function(e){
+
+        if($(e.target).attr('id') === "modal" || $(e.target).hasClass("closeModal")){// проверка события клика
+    
+                modal.hideModal();
+           
+           }else{
+            return false;
+           }
+
+
+    });
