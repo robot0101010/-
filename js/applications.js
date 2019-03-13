aba38f883b418ec3b494b10e65c7037de9ae1f9b
robot0101010 <tt.tt.tt.tt.tt.tt.tt@yandex.ru>
2019-03-13 03:45:24 +0300

    no message

---
+++ js/applications.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+
+
+$(".owl-carousel").owlCarousel({
+
+ nav:true,
+ loop:true,
+ autoplay:true,
+ autoplayTimeout: 2000,
+ animateOut: 'fadeOut',
+ item:1,
+});
